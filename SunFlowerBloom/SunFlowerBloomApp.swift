4f38b4fcbdb10d34a2425d4ce3915326e24fdd98
Vishal Paliwal <paliwalvishal16@gmail.com>
2023-05-06 15:16:31 +0530

    Initial Commit

---
+++ SunFlowerBloom/SunFlowerBloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunFlowerBloomApp.swift
+//  SunFlowerBloom
+//
+//  Created by Vishal Paliwal on 06/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SunFlowerBloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
